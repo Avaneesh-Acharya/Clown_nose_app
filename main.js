4caba346e758ee26f49721fcebdad352c2764a23
Avaneesh-Acharya <137638840+Avaneesh-Acharya@users.noreply.github.com>
2023-07-08 11:18:39 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+noseX=""
+noseY=""
+function preload() {    
+    clown_nose=loadImage("https://i.postimg.cc/4NKV2vBM/467432-middle-removebg-preview.png")
+}
+function setup() {
+    canvas=createCanvas(890, 680)
+    canvas.center()
+    video=createCapture(VIDEO)
+    video.size(890, 680)
+    video.hide()
+    pn=ml5.poseNet(video, Model_Loaded())
+    pn.on('pose', Got_Pose)
+}
+function Got_Pose(Results) {
+    if (Results.length>0) {
+        console.log(Results)
+        console.log("nose x="+Results[0].pose.nose.x)
+        console.log("nose y="+Results[0].pose.nose.y)
+        noseX=Results[0].pose.nose.x
+        noseY=Results[0].pose.nose.y
+    }   
+}
+function Model_Loaded() {
+    console.log("POSE NET IS INITIALIZED")
+}
+function draw() {
+    image(video, 0, 0, 890, 680)
+    fill(255,0,0)
+    stroke(0,25, 37)
+    //circle(noseX, noseY, 15)//
+    image(clown_nose, noseX-28, noseY-2.23, 65,65)
+}
+function take_snapshot() {
+    save("MYfilterimage.png")
+}
